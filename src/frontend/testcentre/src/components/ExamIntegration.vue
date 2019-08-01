df0d6b4e6573a61ec11c5f457cacd7efd194c3e2
Cameron Tod <ctod@amazon.com>
2019-08-02 00:19:59 +1200

    Added task token handling.

--- src/frontend/testcentre/src/components/ExamIntegration.vue
+++ src/frontend/testcentre/src/components/ExamIntegration.vue
@@ -7,22 +7,29 @@
           <div class="control">
             <div class="tags has-addons">
               <span class="tag">Incident ID</span>
-              <span class="tag is-link is-info">{{examData.IncidentId}}</span>
+              <span class="tag is-info">{{examData.IncidentId}}</span>
             </div>
           </div>
           <div class="control">
             <div class="tags has-addons">
               <span class="tag">Exam ID</span>
-              <span class="tag is-link is-warning">{{examData.ExamId}}</span>
+              <span class="tag is-warning">{{examData.ExamId}}</span>
+            </div>
+          </div>
+          <div class="control">
+            <div class="tags has-addons">
+              <span class="tag">Task Token</span>
+              <span id="task-token-preview" class="tag is-danger">{{examData.TaskToken}}</span>
             </div>
           </div>
           <hr />
         </div>
         <p>
-          This application expects two GET variables to be passed in the URL:
+          This application expects three GET variables to be passed in the URL:
             <ul>
               <li><span class="is-family-monospace">IncidentId</span> - the Incident this attempt relates to.</li>
               <li><span class="is-family-monospace">ExamId</span> - the unique identifier for this particular attempt.</li>
+              <li><span class="is-family-monospace">TaskToken</span> - a callback task token for this Step Function execution.</li>
             </ul>
         </p>
         <p>
@@ -33,11 +40,12 @@
       <div class="level">
         <div class="level-left">
           <div class="control level-item">
-              <a class="button is-info" href="?IncidentId=foo123&ExamId=bar456">Test GET variables</a>
+              <a class="button is-info" href="?IncidentId=foo123&ExamId=bar456&TaskToken=AAAAKgAAAAIAAAAAAAAAASWo0UxfuXhTgRdqc1rySg1PZc/6rKbRHisQGn/pi89D8fIVujH6v3RS2DnWLIyPkgfJnI0H+WkRFoz8wXhfEgVvz6xRHEPURX2oi/eMINBFbGev89Kydex7Q3fi0mELPrD/gyfKsKwwsPPjXRPj1G4sDXwP3tZYdKGD/f1GfR04IIUB1uAKHSu6zTeQglzm+6cTbxAcGqcBgg6aIcUCY2PoA0Rk+mLzu/Y7+rY0FwQQCl0Npb8R8VLzEC2guM67oDFd9pKWbhEDkB700wNVQvpYAr6bvHFlbpVtXwoD16T8rZ0c802nvCEl7MvW/t+zxBSxbidGWsvXhF172iWpWcPnsEPUBjrcDglf/6MgRxzNIc4VXmRRp/GYBrEnYU1qf0qkeOQb0Fuq30nxtOvRcYp+WwFOMNXDpezhgu4LGzYT2J69+0mljvRRGNMXt0hG52fysjL53j4IMxZF/Hqy3ZW+DlkbFW38ePZufuBwsuT2oUbc1IR3+W5pZZ0OAuJLWDjHQRAsDVEaVQyjsHMynm13wFYPUvFXUorORLX9Y8fI8Fphp71ZDMPvbtqAhfc4Z25x08abQXSMKoXsmwNAaQssvyT42qXO79UnlfbsgALavU6Q2oLgUSmAX0Ou9lhgIVYhHfZDvX9lGTnlRmFdhz4">
+              Test GET variables</a>
           </div>
 
           <div class="control level-item">
-              <a @click="submitToStepFunctions" class="button is-black" href="?IncidentId=foo123&ExamId=bar456">Submit to Step Function Execution</a>
+              <a @click="submitToStepFunctions" class="button is-black" href="#">Submit to Step Function Execution</a>
           </div>
         </div>
       </div>
@@ -47,7 +55,7 @@
         of a POST to our APIGW endpoint.
       -->
 
-      <div  class="notification success exam-submitted is-primary">
+      <div v-if='submitSuccessful' class="notification success exam-submitted is-primary">
         <button v-on:click="deleteNotification" class="delete"></button>
         Your exam has been submitted. Your score was <strong>{{examData.Score}}%</strong> Your assessor will be in touch to let you
         know what the next steps are.
@@ -78,6 +86,8 @@ export default {
         IncidentId: 'Not supplied',
         // Unique identifier for exam attempt.
         ExamId: 'Not supplied',
+        // Task Token unique to the current Step Functions execution.
+        TaskToken: 'Not supplied'
       },
       // Set to true if a POST to our API is successful.
       submitSuccessful: false,
@@ -90,10 +100,11 @@ export default {
   // Keys can get passed as a GET variables,
   // in the form ?IncidentId=foo&ExamId=bar
   created: function() {
-      let uri = window.location.search.substring(1);
+      let uri = window.location.search;
       let params = new URLSearchParams(uri);
       this.examData.IncidentId = params.get('IncidentId') || this.examData.IncidentId;
       this.examData.ExamId = params.get('ExamId') || this.examData.ExamId;
+      this.examData.TaskToken = params.get('TaskToken') || this.examData.TaskToken;
   },
 
   mounted: function() {
