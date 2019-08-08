f1feae1c334775993d880202ede37996881feb65
Stephen Liedig <sliedig@amazon.com>
2019-08-08 23:49:15 +0800

    Removed ValidateExamTask task and moved business logic into the Incident State class

--- src/backend/IncidentState/State.cs
+++ src/backend/IncidentState/State.cs
@@ -3,52 +3,75 @@ using System.Collections.Generic;
 
 namespace IncidentState
 {
-  
-  public class StateWrapper
-  {
-    public State Input { get; set; }
-    public string TaskToken { get; set; }
-  }  
-  
-  public class State
-  {
-    public string StudentId { get; set; }
-    public Guid IncidentId { get; set; }
-    public DateTime IncidentDate { get; set; }
-    public List<Exam> Exams { get; set; }
-    public DateTime? ResolutionDate { get; set; }
-    public bool IncidentResolved { get; set; }
-    public bool AdminActionRequired { get; set; }
-    
-  }
-
-  public class Exam
-  {
-    public Guid ExamId { get; set; }
-    public DateTime ExamDate { get; set; }
-    public int Score { get; set; }
-    public ExamResult Result { get; set; }
-    public bool NotificationSent { get; set; }
-
-    public Exam(Guid examId, DateTime examDate, int score)
+    public class StateWrapper
     {
-      ExamId = examId;
-      ExamDate = examDate;
-      Score = score;
-      NotificationSent = false;
+        public State Input { get; set; }
+        public string TaskToken { get; set; }
     }
 
-    public override string ToString()
+    public class State
     {
-      return $"ExamId: {ExamId}, ExamDate: {ExamDate}, Score: {Score}, Result: {Result}, NotificationSent: {NotificationSent}";
+        public string StudentId { get; set; }
+        public Guid IncidentId { get; set; }
+        public DateTime IncidentDate { get; set; }
+        public List<Exam> Exams { get; set; }
+        public DateTime? ResolutionDate { get; set; }
+        public bool IncidentResolved { get; set; }
+        public bool AdminActionRequired { get; set; }
     }
-  }
 
-  public enum ExamResult
-  {
-    Pass,
-    Fail,
-    DidNotSitExam
-  }
+    public class Exam
+    {
+        private ExamResult _examResult;
+        public Guid ExamId { get; set; }
+        public DateTime ExamDate { get; set; }
+        public int Score { get; set; }
 
+        public ExamResult Result
+        {
+            get
+            {
+                if (Score >= 76)
+                {
+                    return _examResult = ExamResult.Pass;
+                }
+
+                if (Score >= 1 & Score < 76)
+                {
+                    return _examResult = ExamResult.Fail;
+                }
+
+                return _examResult = ExamResult.DidNotSitExam;
+            }
+
+            set => _examResult = value;
+        }
+        
+        public bool NotificationSent { get; set; }
+
+        public Exam()
+        {
+        }
+
+        public Exam(Guid examId, DateTime examDate, int score)
+        {
+            ExamId = examId;
+            ExamDate = examDate;
+            Score = score;
+            NotificationSent = false;
+        }
+
+        public override string ToString()
+        {
+            return
+                $"ExamId: {ExamId}, ExamDate: {ExamDate}, Score: {Score}, Result: {Result}, NotificationSent: {NotificationSent}";
+        }
+    }
+
+    public enum ExamResult
+    {
+        Pass,
+        Fail,
+        DidNotSitExam
+    }
 }
