96bb0325b93b122432d8143b71eafc3eeb24df82
kevinwochan <kevinwochan@gmail.com>
2023-12-21 21:42:16 +1100

    removing vuejs implementation

--- src/frontend-vue/testcentre/replace-endpoint.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Used during build to replace the APIGW endpoint value provided to Vue.
-# Call like `replace-endpoint.sh foo`, and .env will contain:
-# VUE_APP_APIGW_ENDPOINT=foo
-echo "VUE_APP_APIGW_ENDPOINT=$1" > .env
