9ecf3b075f0c689cac2c9928888f123cbca23943
Stephen Liedig <sliedig@amazon.com>
2024-07-22 15:20:54 +0800

    fix: added default constructors to help with StepFunction serialization.

--- src/backend/Plagiarism/IncidentNotFoundException.cs
+++ src/backend/Plagiarism/IncidentNotFoundException.cs
@@ -1,14 +1,17 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
 using System;
 
-namespace Plagiarism
+namespace Plagiarism;
+
+public class IncidentNotFoundException : Exception
 {
-    public class IncidentNotFoundException : Exception
+    public IncidentNotFoundException()
     {
-        public IncidentNotFoundException()
-        { 
-        }
-        public IncidentNotFoundException(string message) : base((string) message)
-        {
-        }
+    }
+
+    public IncidentNotFoundException(string message) : base((string)message)
+    {
     }
 }
