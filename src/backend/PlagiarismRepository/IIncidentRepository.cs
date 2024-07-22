ba67c7961369ea84135fd725bfc881464f10b170
Stephen Liedig <sliedig@amazon.com>
2024-07-22 15:16:24 +0800

    feat: updated repository and repository tests. Local testing with DDB Local

--- src/backend/PlagiarismRepository/IIncidentRepository.cs
+++ src/backend/PlagiarismRepository/IIncidentRepository.cs
@@ -1,11 +1,24 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
 using System;
 using Plagiarism;
 
-namespace PlagiarismRepository
+namespace PlagiarismRepository;
+
+public interface IIncidentRepository
 {
-    public interface IIncidentRepository
-    {
-        Incident SaveIncident(Incident incident);
-        Incident GetIncidentById(Guid incidentId);
-    }
+    /// <summary>
+    /// Saves incident
+    /// </summary>
+    /// <param name="incident">Incident instance</param>
+    /// <returns>Incident instance saved to the table</returns>
+    Incident SaveIncident(Incident incident);
+
+    /// <summary>
+    /// Gets incident by id
+    /// </summary>
+    /// <param name="incidentId">Incident Id</param>
+    /// <returns>Incident instance</returns>
+    Incident GetIncidentById(Guid incidentId);
 }
