8c2da199c24d4d9ac46c4327b27fcd45108830dd
Stephen Liedig <sliedig@amazon.com>
2024-09-23 17:22:24 +0800

    fix: removed runtime from description

--- src/frontend/src/app/admin/page.tsx
+++ src/frontend/src/app/admin/page.tsx
@@ -90,7 +90,7 @@ export default function AdminPage({ }) {
                                 before administrative action is taken.</p>
 
                             <p>This demo uses exposes an <a href="https://aws.amazon.com/step-functions/">AWS Step Function</a>  via an <a href="https://aws.amazon.com/api-gateway/">Amazon API Gateway</a>. The step-function definition invokes
-                                tasks via <a href="https://aws.amazon.com/lambda/">AWS Lambda</a> (Python 3.6), that store results in <a href="https://aws.amazon.com/dynamodb">Amazon DynamoDB</a>. Notifications are implemented
+                                tasks via <a href="https://aws.amazon.com/lambda/">AWS Lambda</a>, that store results in <a href="https://aws.amazon.com/dynamodb">Amazon DynamoDB</a>. Notifications are implemented
                                 via <a href="https://aws.amazon.com/dynamodb">Amazon SNS</a> and <a href="https://aws.amazon.com/xray/">AWS X-Ray</a> provides distributed tracing capability.</p>
                         </div>
                     </div>
